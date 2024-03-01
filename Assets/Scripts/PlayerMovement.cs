a9c354c895a6cfd3134b803277149e42202302ee
Eduardo Villar <alu.142886@usj.es>
2024-03-01 17:24:28 +0100

    movement tweaks / MRUA animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,6 @@
 //#define USE_MRU
-//#define USE_MRUA
-#define USE_RIGIDBODY
+#define USE_MRUA
+//#define USE_RIGIDBODY
 
 using System;
 using System.Collections;
@@ -11,38 +11,59 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Animator m_Animator;
-
+    private Rigidbody m_Rigidbody;
     public PlayerInput m_PlayerInput;
 
+    [Header("Input variables")]
     //private bool m_ShootPressed = false;
     public Vector2 m_MoveInput = Vector2.zero;
     public bool m_ShiftPressed = false;
 
-    public float m_Speed = 10f;
-    public float m_SpeedMultiplier = 2f;
-    public float m_MaxSpeed = 10.0f;
-    public float m_Acceleration = 1.0f;
-    public float m_Deceleration = 1.0f;
 
+    [Header("Speed variables")]
+    public float m_Speed = 10f;
+    public float m_SpeedMultiplier = 1f;
+    public float m_MaxSpeed = 10.0f;
     public float m_RotationSpeed = 0f;
     public float m_MaxRotationSpeed;
+    //añadido por mi
+    public float m_MaxRunningSpeed;
+
+    [Header("Acceleration variables")]
+    public float m_Acceleration = 1.0f;
+    public float m_Deceleration = 1.0f;
     public float m_RotationAcceleration = 45f;
     public float m_RotationDamping;
 
-    private Rigidbody m_Rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         m_Animator = GetComponent<Animator>();
 
+#if USE_MRU
+        m_Speed = 10f;
+        m_RotationSpeed = 360f;
+
+#elif USE_MRUA
+        m_Speed = 0f;
+        m_Acceleration = 5.0f;
+        m_Deceleration = 5.0f;
+        m_SpeedMultiplier = 1f;
+        m_RotationSpeed = 0f;
+        m_MaxRunningSpeed = 10f;
+        m_MaxSpeed = m_MaxRunningSpeed/2;
+        m_MaxRotationSpeed = 180f;
+        m_RotationDamping = 10.0f;
+        m_RotationAcceleration = 360f;
+
+#elif USE_RIGIDBODY
         m_Speed = 10000f;
         m_SpeedMultiplier = 1f;
         m_RotationSpeed = 10000f;
-        m_MaxSpeed = 10000f;
-        m_MaxRotationSpeed = 11180.0f;
-        m_RotationDamping = 10.0f;
-        m_RotationAcceleration = 90.0f;
+
+#endif
+
         m_PlayerInput = GetComponent<PlayerInput>();
         m_Rigidbody = GetComponent<Rigidbody>();
     }
@@ -62,6 +83,9 @@ public class PlayerMovement : MonoBehaviour
 
     private void AnimationControl()
     {
+
+#if USE_MRU || USE_RIGIDBODY
+
         float speed = 0;
         if (Input.GetKey(KeyCode.W))
         {
@@ -73,6 +97,13 @@ public class PlayerMovement : MonoBehaviour
             }
         }
         m_Animator.SetFloat("MovementSpeed", speed);
+
+#elif USE_MRUA
+
+        m_Animator.SetFloat("MovementSpeed", (m_Speed/m_MaxRunningSpeed));
+
+#endif
+
     }
     private void Inputs()
     {
@@ -102,7 +133,11 @@ public class PlayerMovement : MonoBehaviour
         if(forwardMovement!=0)
         {
             m_Speed += m_Acceleration * forwardMovement * dt;
-            Mathf.Clamp(m_Speed, -m_MaxSpeed, m_MaxSpeed);
+            //[LO HECHO EN CLASE]
+            //m_Speed = Mathf.Clamp(m_Speed, -m_MaxRunningSpeed, m_MaxRunningSpeed);
+
+            //[MI VERSION] Si el shift esta presionado, la velocidad hace un lerp hasta la max running speed, si no, lo hace hasta la maxspeed normal
+            m_Speed = m_ShiftPressed ? Mathf.Lerp(m_Speed, m_MaxRunningSpeed, m_Acceleration * dt) : Mathf.Lerp(m_Speed, m_MaxSpeed, m_Acceleration * dt);
         }
         else
         {
@@ -143,7 +178,7 @@ public class PlayerMovement : MonoBehaviour
         if(horizontalMovement != 0)
         {
             m_RotationSpeed += horizontalMovement * m_RotationAcceleration * dt;
-            Mathf.Clamp(m_RotationSpeed, -m_MaxRotationSpeed, m_MaxRotationSpeed);
+            m_RotationSpeed = Mathf.Clamp(m_RotationSpeed, -m_MaxRotationSpeed, m_MaxRotationSpeed);
         }
         else
         {
